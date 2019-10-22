f2bae70b295f0184b9eb0383b6cc62baf991a0d7
TadeuA <tadeu01399@gmail.com>
2019-10-22 11:49:34 -0300

    news configurations server and routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,14 @@
 // Importar dependencias externas
 const express = require('express');
 
+//Importar regras de nogócios
+const IngredientController = require('./controller/Ingredient/index');
+
 // definindo responsavel pelas rotas
 const routes = express.Router();
 
+//definir rotas
+routes.post('/ingredients', IngredientController.store);
 
 // exportar esse arquivo
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 // Importar modulos externos
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 // Importar modulos internos
+const configMongo = require('./config/databaseNoSQL');
+
+//Conectar ao Servidor db
+mongoose.connect(configMongo,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
   
   //Importar rotas
 const routes = require('./routes');

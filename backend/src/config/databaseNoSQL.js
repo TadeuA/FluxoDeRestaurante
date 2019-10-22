dd8e600b648ca1ead558ccb9d09a1a4a74921e8a
TadeuA <tadeu01399@gmail.com>
2019-10-22 11:46:37 -0300

    edit config mongo

--- backend/src/config/databaseNoSQL.js
+++ backend/src/config/databaseNoSQL.js
@@ -1,13 +1,7 @@
 //definir usuario e senha Mongodb
 const user = "aluno";
 const password = "alunotadeu";
-const host = `mongodb+srv://${user}:${password}@pi-jtump.mongodb.net/test?retryWrites=true&w=majority`,
-const param = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+const host = `mongodb+srv://${user}:${password}@pi-jtump.mongodb.net/test?retryWrites=true&w=majority`;
+
 //configuração de conexão
-module.exports = connect = {
-        host, 
-        param,
-    }
+module.exports = host;

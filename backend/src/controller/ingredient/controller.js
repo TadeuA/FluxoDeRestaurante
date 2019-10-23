61e1422e9501a05c9adc95a17edf1aa5bad76276
TadeuA <tadeu01399@gmail.com>
2019-10-23 10:30:57 -0300

    CRUD Ingredients

--- backend/src/controller/ingredient/controller.js
+++ backend/src/controller/ingredient/controller.js
@@ -5,22 +5,46 @@ const Ingredient = require(path.resolve(__dirname,"..","..","models","Ingredient
 
 //exportar controller
 module.exports = {
-    async store(req,res){
+    //Criar ingredients
+    async store( req, res ){
         //Busca o name de dentro da requisição de forma abstrada
         const { name } = req.body;
         
         //vereficação para ver se o ingrediente ja existe e se não existir cria-o
         let ingredient = await Ingredient.findOne({ name });
-        if(!ingredient){
+        if( !ingredient ){
             ingredient = await Ingredient.create({ name });
         }
 
-        return res.json(ingredient);
+        return res.json( ingredient );
     },
 
-    async index(req,res){
+     // listar ingredients
+    async index( req, res ){
+       
         const ingredients = await Ingredient.find();
 
-        return res.json(ingredients);
-    }
-}
+        return res.json( ingredients );
+    },
+    
+    // Buscar um ingredient
+    async show( req, res ){
+        const ingredient = await Ingredient.findById( req.params.id );
+
+        return res.json( ingredient );
+    }, 
+
+    //Atualizar ingredient
+    async update( req, res ){
+        const ingredient = await Ingredient.findByIdAndUpdate( req.params.id, req.body, { new: true } );
+        
+        return res.json( ingredient );
+    },
+
+    //Deletar Ingredient
+    async destroy( req, res ){
+        await Ingredient.findByIdAndRemove( req.params.id );
+
+        return res.send();
+    },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,16 +2,22 @@
 const express = require('express');
 
 //Importar regras de nogócios
+  //Ingredients
 const IngredientController = require('./controller/ingredient/controller');
+  //Dish
 const DishController = require('./controller/dish/controller');
 
 // definindo responsavel pelas rotas
 const routes = express.Router();
 
 //definir rotas
-routes.post('/ingredients', IngredientController.store);
-routes.get('/ingredients', IngredientController.index);
-
+  // ingredients
+routes.post('/ingredients', IngredientController.store);//criar          c  
+routes.get('/ingredients', IngredientController.index);//listar          r
+routes.get('/ingredients/:id', IngredientController.show);//mostrar      r
+routes.put('/ingredients/:id', IngredientController.update);//atualizar  u
+routes.delete('/ingredients/:id', IngredientController.destroy);//deletar   d  
+  //Dish
 routes.post('/dish', DishController.store);
 
 

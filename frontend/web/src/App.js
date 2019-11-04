9405973f297d9af93d534eab358c17ed93e35a94
TadeuA <tadeu01399@gmail.com//>
2019-11-03 21:21:43 -0300

    view navbar

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./App.css";
 
+import menu from "./assets/button/menu.svg";
+import ingredients from "./assets/button/ingredients.svg";
+import order from "./assets/button/order.svg";
+import dish from "./assets/button/dish.svg";
+
 const App = () => (
   <div className="App">
-    <h1>Hello word!</h1>
+    <div id="navBar">
+      <button id="profile" className="btn"></button>
+      <button className="btn">
+        <img src={order} alt="menu" />
+      </button>
+      <button className="btn">
+        <img src={menu} alt="menu" />
+      </button>
+      <button className="btn">
+        <img src={dish} alt="menu" />
+      </button>
+      <button className="btn">
+        <img src={ingredients} alt="menu" />
+      </button>
+    </div>
   </div>
 );
 

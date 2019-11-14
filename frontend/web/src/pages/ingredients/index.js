fd62b883d7bfdf7fff909a8bc857660c427ce0ee
TadeuA <tadeu01399@gmail.com//>
2019-11-14 00:37:00 -0300

    classification ingredient

--- frontend/web/src/pages/ingredients/index.js
+++ frontend/web/src/pages/ingredients/index.js
@@ -11,21 +11,36 @@ export default function Ingredients() {
   const [ingredient, setIngredient] = useState([]);
   const [dishs, setDishs] = useState([]);
   const [name, setName] = useState("");
+  const [classifications, setClassifications] = useState([])
+  const [classification, setClassification] = useState("")
 
   //listar ingredientes
   const listIngredients = (
     <ul className="ingredient-list">
-      {ingredient.map(ingre => (
-        <li key={ingre._id}>
-          <h5>{ingre.name}</h5>
-          <button className="direction" onClick={() => handleDishs(ingre._id)}>
-            <img src={direction} alt="direction" />
-          </button>
-          <button className="destroy" onClick={() => handleDestroy(ingre)}>
-            <img src={destroy} alt="destroy" />
-          </button>
+      {classifications.map(item => {
+        if(item.section ==="ingredient"){
+          return(
+          <li key={item._id}>
+            <span>{item.classification}</span>
+            <ul>
+            {ingredient.map(subItem => {
+
+              if(subItem.classification._id === item._id){
+                return(
+                  <li key={subItem._id}>
+                    <h5>{subItem.name}</h5>
+                    <button className="direction" onClick={() => handleDishs(subItem._id)}>
+                      <img src={direction} alt="direction" />
+                  </button>
+                  <button className="destroy" onClick={() => handleDestroy(subItem)}>
+                    <img src={destroy} alt="destroy" />
+                  </button>
+                </li>
+      )}})}
+          </ul>
         </li>
-      ))}
+      )}})}
+
     </ul>
   );
 
@@ -52,9 +67,11 @@ export default function Ingredients() {
   //criar novo ingrediente
   async function handleSubmit(event) {
     event.preventDefault();
-    const response = await api.post(`/ingredients`, { name });
+
+    const response = await api.post(`/ingredients`, { name, classification });
     ingredient.push(response.data)
-    setIngredient(ingredient);
+    localStorage.setItem("ingredients", JSON.stringify(ingredient))
+    loadIngredients()
     window.alert(`${name} adicionado a lista de ingredientes!`);
     setName("");
   }
@@ -64,6 +81,7 @@ export default function Ingredients() {
   }
   useEffect(() => {
     loadIngredients();
+    setClassifications(JSON.parse(localStorage.getItem("classification")))
   }, []);
 
   //apagar ingrediente
@@ -132,7 +150,19 @@ export default function Ingredients() {
                   value={name}
                   onChange={event => setName(event.target.value)}
                 />
+
               </fieldset>
+              <label>Defina a Classificação</label>
+              <select onChange={event => setClassification(event.target.value)}>
+                  <option value=""></option>
+                  {classifications.map(item => {
+                    if(item.section === "ingredient"){
+                      return (
+                      <option key={item._id}value={item._id}>{item.classification}</option>
+                      )
+                    }
+                  })}
+                </select>
               <button type="submit">Adicionar Novo</button>
             </form>
           </div>
